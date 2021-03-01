910c9ba68f99291eb9ae43d133f9c428e8f22643
Rainier Hasbrouck <57506624+RainierEH@users.noreply.github.com>
2021-03-01 10:23:45 -0800

    Update rainierMHplan.py

--- Monty Hall Problem/rainierMHplan.py
+++ Monty Hall Problem/rainierMHplan.py
@@ -3,3 +3,21 @@
 # Last modified 3/1/2021
 # Pseudocode for the Monty Hall problem. This stuff hurts my brain :P
 
+doors = {
+    '1' : ['1', '0', '0']
+    '2' : ['0', '1', '0']
+    '3' : ['0', '0', '1']
+    }
+
+Make a dictionary "doors" containing lists with possible door configurations
+
+Select a random configuration from the dictionary
+
+Ask the user to pick a door
+
+If the door is a goat, reveal the location of the other goat
+Otherwise, if the door is the car, pick any goat to reveal
+
+Ask the user if they would like to switch the door
+
+Reveal the car location, and tell the user what they picked.
