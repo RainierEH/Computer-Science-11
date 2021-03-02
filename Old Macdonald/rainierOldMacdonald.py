173987f4f0d0f23e0f2901535c59ce79461b3e3e
RainierEH <swc.rainier@gmail.com>
2021-03-02 10:04:25 -0800

    Macdonald workinnnn lets go. Time to work on distance I think, then get MH working at home as it's overdue. Yikes.

---
+++ Old Macdonald/rainierOldMacdonald.py
@@ -0,0 +1,18 @@
+# Rainier Hasbrouck
+# Created 3/2/2021
+# Last modified 3/2/2021
+# Sings a rendition of the Old Macdonald Had a Farm song.
+
+
+def sing(animal, noise):
+    print('Old Macdonald had a farm, e-i-e-i-o!')
+    print('And on this farm there was a ' + animal + ', e-i-e-i-o!')
+    print('With a ' + noise + ' ' + noise + ' here and a ' + noise + ' ' + noise + ' there, Here a ' + noise + ', there a ' + noise + ', everywhere a ' + noise + ' ' + noise + '!')
+    print('Old Macdonald had a farm, e-i-e-i-o!')
+    print('')
+
+
+sing('duck', 'quack')
+sing('cow', 'moo')
+sing('mutalisk', 'woosh')
+sing('cat', 'meow')
