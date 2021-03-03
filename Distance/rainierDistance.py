f68d85bf8c40851afaf2d8a7d657b0077a754eaf
RainierEH <swc.rainier@gmail.com>
2021-03-03 08:47:03 -0800

    PEP 8: E69420, Some stupid bullsh*t

--- Distance/rainierDistance.py
+++ Distance/rainierDistance.py
@@ -4,7 +4,10 @@
 # Calculates the distance between two coordinates on a Cartesian plane.
 from math import sqrt
 
+
 def distance(x1, x2, y1, y2):
     return sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
+
 print(distance(0, 0, 3, 4))
+
